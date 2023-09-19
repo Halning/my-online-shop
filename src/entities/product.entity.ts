160f7bc067f55e5733afe5f3549f0c1051d32cbd
andriikhomenko <icandrii.khomenko@americanwell.com>
2023-09-19 14:12:45 +0300

    Remove dead code

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -14,17 +14,3 @@ export class Product {
     @Property()
     price!: number;
 }
-
-// export interface ProductEntity {
-//     id: string; // uuid
-//     title: string;
-//     description: string;
-//     price: number;
-// }
-//
-// export const product: ProductEntity = {
-//     id: '51422fcd-0366-4186-ad5b-c23059b6f64f',
-//     title: 'Book',
-//     description: 'A very interesting book',
-//     price: 100
-// }
